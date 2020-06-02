9c9c7a087978f4b5cfe2304fb29dfbbba2b1f123
Victor Daniel <nicolaevictordaniel@gmail.com>
2020-06-02 15:25:50 +0200

    Ejercicios Repaso

---
+++ Poligonos/Cuadrado.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poligonos
+{
+    /// <summary>
+    /// Area que devuelva el area de un cuadrado
+    /// </summary>
+    public class Cuadrado
+    {
+        public double Lado { get; set; }
+        public double Area 
+        {
+            get
+            {
+                return Lado * Lado;
+            }
+        }
+    }
+}
